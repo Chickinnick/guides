9b4113b067c561bfd646b33e98b7f1ff9429bb73
Mykola Dymura <mykola.dymura@globallogic.com>
2015-03-23 12:35:38 +0200

    Added example for work with UI

---
+++ ui/src/main/java/org/guides/ui/app/MainActivity.java
@@ -0,0 +1,25 @@
+package org.guides.ui.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.text_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TextViewActivity.class));
+            }
+        });
+    }
+}
